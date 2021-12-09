f11e98d4efb01c7a9e483892414a5ac74488f2e4
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-12-09 12:05:09 -0500

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -32082,7 +32082,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59998" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50238" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
